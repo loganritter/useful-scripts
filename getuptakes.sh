cce36277149e4047fcb8988973ec12f256263051
loganritter <32370525+loganritter@users.noreply.github.com>
2023-07-24 15:56:33 -0400

    Create getuptakes.sh
    Extracts the uptakes from a MPMC job and converts wt% to cm3/g (can easily change to be mmol/g if desired)

---
+++ getuptakes.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+base=$(pwd)
+
+for temp in 220 230 240 250; do
+        cd $temp
+
+        echo ""
+        echo $temp
+        echo "kPa cm3/g"
+        echo "0.0 0.0"
+
+        for pressure in 0.0003 0.0005 0.0008 0.001 0.002; do
+                cd $pressure
+
+                kpa=`echo "$pressure * 101.3" | bc -l`
+                mmol=`grep "wt % (ME) =" runlog.log | awk 'END{print ($6*10)/(131.293)*22.4}'`
+
+                echo $kpa $mmol
+
+                cd ../
+        done
+        cd ../
+done
