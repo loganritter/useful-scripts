830aef1252899ee734638dda75e7038adfe012f8
loganritter <32370525+loganritter@users.noreply.github.com>
2023-07-24 15:59:25 -0400

    Update getuptakes.sh

--- getuptakes.sh
+++ getuptakes.sh
@@ -14,9 +14,9 @@ for temp in 220 230 240 250; do
                 cd $pressure
 
                 kpa=`echo "$pressure * 101.3" | bc -l`
-                mmol=`grep "wt % (ME) =" runlog.log | awk 'END{print ($6*10)/(131.293)*22.4}'`
+                cm3=`grep "wt % (ME) =" runlog.log | awk 'END{print ($6*10)/(131.293)*22.4}'`
 
-                echo $kpa $mmol
+                echo $kpa $cm3
 
                 cd ../
         done
