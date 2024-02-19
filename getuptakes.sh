6d05a901f361c9e876d1569b7d41f2697e9e5ac3
loganritter <32370525+loganritter@users.noreply.github.com>
2024-02-19 14:11:27 -0500

    Update getuptakes.sh

--- getuptakes.sh
+++ getuptakes.sh
@@ -2,23 +2,39 @@
 
 base=$(pwd)
 
-for temp in 220 230 240 250; do
-        cd $temp
+for gas in h2 n2; do
+	cd $gas
+	for temp in 77; do
+		cd $temp
+	
+		echo ""
+		echo $gas $temp
+		echo "atm wtp mmol/g mmol/mmol kJ/mol N"
+	
+	        for pressure in 0.001 0.005 0.01 0.03 0.05 0.07 0.09 `seq 0.1 0.1 1.0`; do
+			cd $pressure
+			
+			if [[ $gas == "h2" ]]; then	
+				wtp=`grep "wt % (ME) =" runlog.log | tail -1 | awk {'print $6'}`
+				mmolg=`grep "wt % (ME) =" runlog.log | awk 'END{print ($6*10/2.01594)}'`
+				mmolmmol=`grep "wt % (ME) =" runlog.log | awk 'END{print ($6*10/2.01594/4.290756)}'`
+			fi
 
-        echo ""
-        echo $temp
-        echo "kPa cm3/g"
-        echo "0.0 0.0"
+			if [[ $gas == "n2" ]]; then
+				wtp=`grep "wt % (ME) =" runlog.log | tail -1 | awk {'print $6'}`
+                mmolg=`grep "wt % (ME) =" runlog.log | awk 'END{print ($6*10/28.0134)}'`
+				mmolmmol=`grep "wt % (ME) =" runlog.log | awk 'END{print ($6*10/28.0134/4.290756)}'`
 
-        for pressure in 0.0003 0.0005 0.0008 0.001 0.002; do
-                cd $pressure
+			fi
+	
+			qst=`grep "qst =" runlog.log | tail -1 | awk {'print $4'}`
+			N=`grep "N =" runlog.log | tail -1 | awk {'print $4'}`
 
-                kpa=`echo "$pressure * 101.3" | bc -l`
-                cm3=`grep "wt % (ME) =" runlog.log | awk 'END{print ($6*10)/(131.293)*22.4}'`
-
-                echo $kpa $cm3
-
-                cd ../
-        done
-        cd ../
+			echo $pressure $wtp $mmolg $mmolmmol $qst $N
+		
+			cd ../
+		done
+		cd ../
+	done
+	cd ../
 done
